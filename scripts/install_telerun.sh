9920ae50932f7157d05a9f4a24f09e8fbf247cb2
Carl Guo <carlguo866@gmail.com>
2024-09-06 09:39:14 -0400

    Update install_telerun.sh

--- scripts/install_telerun.sh
+++ scripts/install_telerun.sh
@@ -4,7 +4,7 @@ set -e
 if [[ $EUID -ne 0 ]]; then
    sudo -E $0
 else
-   telerun_url="https://raw.githubusercontent.com/MIT-6-106/student_software/main/telerun/submit.py"
+   telerun_url="https://raw.githubusercontent.com/6106-fall24/student_software/main/telerun/submit.py"
    echo "Installing telerun"
    mkdir -p /opt/6106
    curl -O --output-dir /opt/6106 $telerun_url
