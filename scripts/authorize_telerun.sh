4f62252df180a358c45e7a09779df33d45fcad09
Carl Guo <carlguo866@gmail.com>
2024-09-11 08:53:32 -0400

    Remove sudo from authorize_telerun

--- scripts/authorize_telerun.sh
+++ scripts/authorize_telerun.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 set -e
 
-if [[ $EUID -ne 0 ]]; then
-   sudo -E $0 "$@"
+USERNAME=
+TOKEN=
+if [ $# -eq 2 ]; then
+   USERNAME="$1"
+   TOKEN="$2"
 else
-   USERNAME=
-   TOKEN=
-
-   if [ $# -eq 2 ]; then
-      USERNAME="$1"
-      TOKEN="$2"
-   else
-      echo "Enter your telerun credentials"
-      echo -n "Username: "
-      read USERNAME
-      echo -n "Token: "
-      read TOKEN
-   fi
-
-   echo $'\nSaving credentials'
-   mkdir -p ~/.telerun
-   echo $'{\n\t"username": "'$USERNAME$'",\n\t"token": "'$TOKEN$'"\n}' > ~/.telerun/auth.json
+   echo "Enter your telerun credentials"
+   echo -n "Username: "
+   read USERNAME
+   echo -n "Token: "
+   read TOKEN
 fi
+echo $'\nSaving credentials'
+mkdir -p "$HOME/.telerun"
+echo $'{\n\t"username": "'$USERNAME$'",\n\t"token": "'$TOKEN$'"\n}' > "$HOME/.telerun/auth.json"
